b0b788acb30ba72c128b59c5c96fd011247759b7
nishant.rajput <nishant.rajput@appventurez.com>
2023-11-27 01:25:10 +0530

    Responsive Nav Started

--- src/app/wrapper/header/header.component.ts
+++ src/app/wrapper/header/header.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatExpansionPanel } from '@angular/material/expansion';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  panelOpenState:boolean=false;
+  panelOpenState: boolean = false;
+  mobileWidth = false;
+  count = 0;
+  @ViewChild('myPanel') myPanel!: MatExpansionPanel;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.breakpointObserver
+      .observe([
+        Breakpoints.HandsetPortrait, // or whichever breakpoint suits your requirement
+        // Breakpoints.HandsetLandscape,
+      ])
+      .subscribe((result) => {
+        if (result.matches) {
+          this.mobileWidth = true;
+        } else {
+          this.mobileWidth = false;
+          this.myPanel.close();
+        }
+      });
+  }
+
+  panelOpened() {
+    if (!this.mobileWidth){
+      this.myPanel.close();
+    } 
+    console.log('Panel opened');
+    // You can add your logic here when the panel is opened
+  }
+
+  panelClosed() {
+    console.log('Panel closed');
+    // You can add your logic here when the panel is closed
+  }
 }
